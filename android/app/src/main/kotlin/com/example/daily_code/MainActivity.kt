1ddb7c1de4452e415225dc35cee91141bae757e4
Danushan99 <danuthasan08@gmail.com>
2025-04-18 20:37:57 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/daily_code/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.daily_code
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/splash_screen/components/splash_view.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
+  final AnimationController animationController;
+
+  const SplashView({Key? key, required this.animationController})
+      : super(key: key);
+
+  @override
+  _SplashViewState createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  Widget build(BuildContext context) {
+    final introductionanimation =
+        Tween<Offset>(begin: const Offset(0, 0), end: const Offset(0.0, -1.0))
+            .animate(CurvedAnimation(
+      parent: widget.animationController,
+      curve: const Interval(
+        0.0,
+        0.2,
+        curve: Curves.fastOutSlowIn,
+      ),
+    ));
+    return SlideTransition(
+      position: introductionanimation,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(
+                top: 120.0,
+              ),
+              child: Text(
+                "Crypto-Gen",
+                style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0, bottom: 10),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  height: 260,
+                  'assets/introduction_animation/locker.png',
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 8.0, bottom: 10.0),
+              child: Text(
+                "Manage all your cryptofolio\nin a single place",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 64, right: 64),
+              child: Text(
+                "With all your cryptocurriencies in one spot, managing them is made simple.",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(
+              height: 48,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).padding.bottom + 16),
+              child: InkWell(
+                onTap: () {
+                  widget.animationController.animateTo(0.2);
+                },
+                child: Container(
+                  height: 58,
+                  padding: const EdgeInsets.only(
+                    left: 56.0,
+                    right: 56.0,
+                    top: 16,
+                    bottom: 16,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(38.0),
+                    color: const Color(0xff132137),
+                  ),
+                  child: const Text(
+                    "Let's begin",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
